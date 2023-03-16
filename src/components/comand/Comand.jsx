5da2c8b51669e28fea95cc0fec383fb0d87fb720
sami1793 <sami_17_94@hotmail.com>
2023-03-16 17:40:16 -0300

    Se agrega boton de copiado de comando

--- src/components/comand/Comand.jsx
+++ src/components/comand/Comand.jsx
@@ -1,9 +1,24 @@
 import './Comand.css'
-export const Comand = ({color, comand, task}) => {
+
+import { FaRegCopy } from 'react-icons/fa'
+import { useState } from 'react'
+
+export const Comand = ({ color, comand, task }) => {
+  const [showCopy, setShowCopy] = useState(false);
+
+  const copyComand = () => {
+    navigator.clipboard.writeText(comand);
+    setShowCopy(true);
+    setTimeout(()=>setShowCopy(false),2000)
+  }
   return (
     <div className={`comandContainer ${color}`}>
+      <div className='comandSelect'>
         <p>{comand}</p>
-        <p>{task}</p>
+        <FaRegCopy onClick={copyComand} />
+        {showCopy&&<p className='copy'>Copiado</p>}
+      </div>
+      <p>{task}</p>
     </div>
   )
 }
